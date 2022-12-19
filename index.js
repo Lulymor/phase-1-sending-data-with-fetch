91a53fa82bd4f3bccb4901a2457892ff0eb8f8e3
Luana <lugamoreira@gmail.com>
2022-12-18 19:07:05 -0500

    commit tree aa10a7cc7ac113d1bcfcd8465a1a523d7c0b4664

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-type': 'application/json'
+        },
+            body: JSON.stringify({
+                'name': name
+                ,
+                'email': email
+            })
+    })        .then(response => response.json())
+    .then(submitData => document.body.append(submitData.id))
+    .catch(error => document.body.append(error))
+}
+
+
+
+
+
+
